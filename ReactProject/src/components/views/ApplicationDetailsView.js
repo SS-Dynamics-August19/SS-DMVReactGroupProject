bf1da0c1fb30530bbbc410ba2fb0da097fd5a1ca
rufatweb <rufat.web@gmail.com>
2019-11-02 11:43:51 -0400

    Changed Appl.Type Digits to Labels

---
+++ ReactProject/src/components/views/ApplicationDetailsView.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+
+
+
+const detailedView = function(props) {
+    return (
+        <div>
+            details
+        </div>
+    );
+}
+
+
+
+
+detailedView.propTypes = {
+    stores: PropTypes.object.isRequired
+};
+
+
+export default view;
