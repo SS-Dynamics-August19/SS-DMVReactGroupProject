900ccc7e2ee34546320bc093b8b0d5c3c53b69c4
rufatweb <rufat.web@gmail.com>
2019-11-03 11:31:00 -0500

    ..

--- ReactProject/sscc.sh
+++ ReactProject/sscc.sh
@@ -1,1 +1,1 @@
-"/Applications/Google Chrome.app/Contents/MacOS/Google Chrome" --user-data-dir="Users/rufataghayev/Desktop/Chrome Cross-Origin Session" --disable-web-security
+./Google\ Chrome --disable-web-security --user-data-dir=Users/rufataghayev/temp/cross-chrome
